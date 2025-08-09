3efa96fff426f7fa3c5d34330624bd9846ab1314
Djal Bertrand <100029664+Djalcoding@users.noreply.github.com>
2025-08-08 20:39:50 -0400

    added color control

--- src/lib.rs
+++ src/lib.rs
@@ -1,59 +1,129 @@
-pub mod UI {
+pub mod ui {
 
     use sysinfo::{Disks, System};
     use tui::{
         Frame,
         backend::Backend,
         layout::{Constraint, Layout},
-        style::{Color, Modifier, Style},
-        widgets::{Block, Borders, Gauge},
+        style::Color,
     };
 
-    pub fn build_gauge(used: u64, total: u64, name: String) -> Gauge<'static> {
-        Gauge::default()
-            .block(
-                Block::default()
-                    .title(format!("{name} USAGE ({used} B / {total} B )",))
-                    .borders(Borders::ALL),
-            )
-            .gauge_style(
-                Style::default()
-                    .fg(Color::White)
-                    .bg(Color::Black)
-                    .add_modifier(Modifier::BOLD),
-            )
-            .percent(((used as f64 / total as f64) * 100.0) as u16)
+    pub mod colors {
+        use std::process;
+
+        use tui::style::Color;
+
+        pub fn get_color_from_string(text: &str) -> Color {
+            match text.to_lowercase().as_str() {
+                "black" => Color::Black,
+                "red" => Color::Red,
+                "green" => Color::Green,
+                "blue" => Color::Yellow,
+                "yellow" => Color::Blue,
+                "magenta" => Color::Magenta,
+                "cyan" => Color::Cyan,
+                "gray" => Color::Gray,
+                "darkgray" => Color::DarkGray,
+                "lightred" => Color::LightRed,
+                "lightgreen" => Color::LightGreen,
+                "lightyellow" => Color::LightYellow,
+                "lightblue" => Color::LightBlue,
+                "lightmagenta" => Color::LightMagenta,
+                "lightcyan" => Color::LightCyan,
+                "white" => Color::White,
+                _ => {
+                    eprintln!("{text} is not a recognized color.");
+                    process::exit(100);
+                }
+            }
+        }
+    }
+    #[allow(dead_code)]
+    mod gauges {
+
+        use tui::{
+            style::{Color, Modifier, Style},
+            widgets::{Block, Borders, Gauge},
+        };
+        pub fn build_gauge(used: u64, total: u64, name: String) -> Gauge<'static> {
+            build_colorful_gauge(used, total, name, Color::White)
+        }
+
+        pub fn build_colorful_gauge(
+            used: u64,
+            total: u64,
+            name: String,
+            color: Color,
+        ) -> Gauge<'static> {
+            Gauge::default()
+                .block(
+                    Block::default()
+                        .title(format!("{name} USAGE ({used} B / {total} B )",))
+                        .borders(Borders::ALL),
+                )
+                .gauge_style(
+                    Style::default()
+                        .fg(color)
+                        .bg(Color::Black)
+                        .add_modifier(Modifier::BOLD),
+                )
+                .percent(((used as f64 / total as f64) * 100.0) as u16)
+        }
+
+        pub fn build_colorful_gauge_percent(
+            used: u16,
+            name: String,
+            color: Color,
+        ) -> Gauge<'static> {
+            Gauge::default()
+                .block(
+                    Block::default()
+                        .title(format!("{name} USAGE ({used}%)",))
+                        .borders(Borders::ALL),
+                )
+                .gauge_style(
+                    Style::default()
+                        .fg(color)
+                        .bg(Color::Black)
+                        .add_modifier(Modifier::BOLD),
+                )
+                .percent(used)
+        }
+
+        pub fn build_gauge_percent(used: u16, name: String) -> Gauge<'static> {
+            build_colorful_gauge_percent(used, name, Color::White)
+        }
     }
 
-    pub fn build_gauge_percent(used: u16, name: String) -> Gauge<'static> {
-        Gauge::default()
-            .block(
-                Block::default()
-                    .title(format!("{name} USAGE ({used}%)",))
-                    .borders(Borders::ALL),
-            )
-            .gauge_style(
-                Style::default()
-                    .fg(Color::White)
-                    .bg(Color::Black)
-                    .add_modifier(Modifier::BOLD),
-            )
-            .percent(used)
-    }
-    pub fn ui<B: Backend>(f: &mut Frame<B>) {
+    use gauges::*;
+    pub fn ui<B: Backend>(f: &mut Frame<B>, color: Color, disk_color: Color) {
         let mut sys = System::new_all();
         sys.refresh_all();
-
         let mut gauge_list = vec![
-            build_gauge(sys.used_memory(), sys.total_memory(), String::from("RAM")), // used RAM
-            build_gauge(sys.used_swap(), sys.total_swap(), String::from("SWAP")),    // used SWAP
-            build_gauge_percent(sys.global_cpu_usage() as u16, String::from("CPU")), // CPU
+            build_colorful_gauge(
+                sys.used_memory(),
+                sys.total_memory(),
+                String::from("RAM"),
+                color,
+            ), // used RAM
+            build_colorful_gauge(
+                sys.used_swap(),
+                sys.total_swap(),
+                String::from("SWAP"),
+                color,
+            ), // used SWAP
+            build_colorful_gauge_percent(sys.global_cpu_usage() as u16, String::from("CPU"), color), // CPU
         ];
 
         let disks = Disks::new_with_refreshed_list();
 
-        for disk in &disks { 
-           gauge_list.push(build_gauge(disk.total_space()- disk.available_space(), disk.total_space(), disk.name().to_str().unwrap_or("Unknow name").to_string())); 
+        for disk in &disks {
+            gauge_list.push(build_colorful_gauge(
+                disk.total_space() - disk.available_space(),
+                disk.total_space(),
+                disk.name().to_str().unwrap_or("Unknow name").to_string(),
+                disk_color,
+            ));
         }
 
         let mut constraints = vec![Constraint::Percentage((100 / gauge_list.len()) as u16)];
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 use std::{
-    io::{self, stdin}, process, sync::mpsc, thread
+    env,
+    io::{self, stdin},
+    process,
+    sync::mpsc,
+    thread,
 };
-use sys_ressources::UI::ui;
+use system_gauges::ui::colors::get_color_from_string;
+use system_gauges::ui::ui;
 
-use termion::{clear, event::Key, input::TermRead, raw::IntoRawMode, screen::IntoAlternateScreen};
-use tui::{Terminal, backend::TermionBackend, terminal};
+use termion::{clear, input::TermRead, raw::IntoRawMode};
+use tui::{Terminal, backend::TermionBackend, style::Color};
 fn main() -> Result<(), io::Error> {
+
+    let gauge_colors = get_colors_from_arguments().unwrap_or_else(|e|{
+        eprintln!("{e}");
+        process::exit(100);
+    });
     //build Terminal
-    let mut stdout = io::stdout().into_raw_mode()?;
+    let stdout = io::stdout().into_raw_mode()?;
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
     println!("{}", clear::All);
@@ -16,20 +26,86 @@ fn main() -> Result<(), io::Error> {
         let stdin = stdin();
         for c in stdin.keys() {
             if let Ok(key) = c {
-                tx.send(key);
+                tx.send(key).err();
             }
         }
     });
 
     loop {
-        if let Ok(..) = rx.try_recv(){
+        // game loop
+        if let Ok(..) = rx.try_recv() {
             break;
         }
         let _ = terminal.draw(|f| {
-            ui(f);
+            ui(f, gauge_colors.0, gauge_colors.1);
         });
     }
 
     println!("{}", clear::All);
     Ok(())
+}
+
+fn get_colors_from_arguments() -> Result<(Color, Color), String> {
+    let arguments: Vec<String> = env::args().collect();
+    let mut color = Color::White;
+    let mut disk_color = Color::White;
+    let mut skip = true;
+    for (i, arg) in arguments.iter().enumerate() {
+        if skip {
+            skip = false;
+            continue;
+        }
+        match arg.as_str() {
+            "-c" => {
+                if i == arguments.len() - 1 {
+                    
+                    return Err(String::from("Please specify a color when you use '-c' "));
+                }
+                color = get_color_from_string(&arguments[i + 1]);
+                skip = true;
+            }
+
+            "-d" => {
+                if i == arguments.len() - 1 {
+                    return Err(String::from("Please specify a color when you use '-d' "));
+
+                }
+                disk_color = get_color_from_string(&arguments[i + 1]);
+                skip = true;
+            }
+
+            "-h" => {
+                let help_menu = "----------------------------------------------
+-h    Show this menu
+-c    Define the color of the default gauges
+-d    Define the color of the disk gauges
+----------------------------------------------
+Color List (case insensitive): 
+
+    black,
+    red,
+    green,
+    yellow,
+    blue,
+    magenta,
+    cyan,
+    gray,
+    darkgray,
+    lightred,
+    lightgreen,
+    lightyellow,
+    lightblue,
+    lightmagenta,
+    lightcyan,
+    white,           ";
+                return Err(format!("{help_menu}"));    
+            }
+            _ => {
+
+                return Err(format!("'{arg}' is not supposed to be there"));    
+            }
+        }
+    }
+
+    Ok((color, disk_color))
 }
