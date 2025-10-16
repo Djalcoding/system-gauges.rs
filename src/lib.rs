29c8fd3c4dea1e2402f96fc2df338690ffc23b77
Djal Bertrand <100029664+Djalcoding@users.noreply.github.com>
2025-10-15 20:25:58 -0400

    Added customization for text and wrapped everything in configuration wrapper

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,10 @@ pub mod ui {
 
     pub mod colors {
         use std::process;
-
         use tui::style::Color;
 
-        pub fn get_color_from_string(text: &str) -> Color {
-            match text.to_lowercase().as_str() {
+        pub fn get_color_from_string(text: &str) -> Result<Color, String> {
+            let color = match text.to_lowercase().as_str() {
                 "black" => Color::Black,
                 "red" => Color::Red,
                 "green" => Color::Green,
@@ -29,21 +28,62 @@ pub mod ui {
                 "lightcyan" => Color::LightCyan,
                 "white" => Color::White,
                 _ => {
-                    eprintln!("{text} is not a recognized color.");
-                    process::exit(100);
+                    return Err(format!("{text} is not a recognized color"));
                 }
-            }
+            };
+
+            Ok(color)
         }
+    }
+
+    pub struct Configuration{
+        gauge_color:Color,
+        disk_color:Color,
+        text_color:Color,
+        borders:Borders,
+        background:bool
+    }
+
+    impl Configuration {
+       pub fn new(gauge_color:Color, disk_color: Color, borders:Borders, background:bool, text_color:Color)->Self{
+            Configuration { gauge_color, disk_color, borders, background, text_color}
+       } 
+
+       fn gauge_color(&self) -> Color{
+            self.gauge_color
+       }
+       fn disk_color(&self) -> Color{
+            self.disk_color
+       }
+       fn borders(&self) -> &Borders{
+            &self.borders
+       }
+
+       fn has_background(&self) -> bool {
+            self.background
+       }
+
+       fn text_color(&self) ->Color {
+           self.text_color
+       }
+    }
+
+    impl Default for Configuration {
+        fn default() -> Self {
+            Configuration { gauge_color: Color::White, disk_color: Color::White, borders: Borders::ALL,  background: true , text_color: Color::White}
+        } 
     }
     #[allow(dead_code)]
     mod gauges {
-
         use tui::{
             style::{Color, Modifier, Style},
-            widgets::{Block, BorderType, Borders, Gauge},
+            widgets::{Block, Borders, Gauge},
         };
+
+        use crate::ui::Configuration;
         pub fn build_gauge(used: u64, total: u64, name: String) -> Gauge<'static> {
-            build_colorful_gauge(used, total, name, Color::White, &Borders::ALL,&true)
+            let config = Configuration::default();
+            build_colorful_gauge(used, total, name, config.gauge_color, config.borders(),config.has_background(), config.text_color)
         }
 
         pub fn build_colorful_gauge(
@@ -52,18 +92,20 @@ pub mod ui {
             name: String,
             color: Color,
             borders:&Borders,
-            background:&bool
+            background:bool,
+            text_color:Color
         ) -> Gauge<'static> {
             Gauge::default()
                 .block(
                     Block::default()
                         .title(format!("{name} USAGE ({used} B / {total} B )",))
-                        .borders(*borders),
+                        .style(Style::default().fg(text_color))
+                        .borders(*borders)
                 )
                 .gauge_style(
                     Style::default()
                         .fg(color)
-                        .bg(if *background {Color::Black} else {Color::Reset})
+                        .bg(if background {Color::Black} else {Color::Reset})
                         .add_modifier(Modifier::BOLD),
                 )
                 .percent(((used as f64 / total as f64) * 100.0) as u16)
@@ -74,30 +116,33 @@ pub mod ui {
             name: String,
             color: Color,
             borders:&Borders,
-            background:&bool
+            background:bool,
+            text_color:Color
         ) -> Gauge<'static> {
             Gauge::default()
                 .block(
                     Block::default()
                         .title(format!("{name} USAGE ({used}%)",))
-                        .borders(*borders),
+                        .style(Style::default().fg(text_color))
+                        .borders(*borders)
                 )
                 .gauge_style(
                     Style::default()
                         .fg(color)
-                        .bg(if *background {Color::Black} else {Color::Reset})
+                        .bg(if background {Color::Black} else {Color::Reset})
                         .add_modifier(Modifier::BOLD),
                 )
                 .percent(used)
         }
 
         pub fn build_gauge_percent(used: u16, name: String) -> Gauge<'static> {
-            build_colorful_gauge_percent(used, name, Color::White, &Borders::ALL, &true)
+            let config = Configuration::default();
+            build_colorful_gauge_percent(used, name, config.gauge_color(), config.borders(), config.has_background(),config.text_color())
         }
     }
 
     use gauges::*;
-    pub fn ui<B: Backend>(f: &mut Frame<B>, color: Color, disk_color: Color, borders:&Borders, background:&bool) { //TODO : Put this into a struct
+    pub fn ui<B: Backend>(f: &mut Frame<B>, config:&Configuration) { //TODO : Put this into a struct
         let mut sys = System::new_all();
         sys.refresh_all();
         let mut gauge_list = vec![
@@ -105,20 +150,31 @@ pub mod ui {
                 sys.used_memory(),
                 sys.total_memory(),
                 String::from("RAM"),
-                color,
-                borders,
-                background
+                config.gauge_color(),
+                config.borders(),
+                config.has_background(),
+                config.text_color()
                 
             ), // used RAM
             build_colorful_gauge(
                 sys.used_swap(),
                 sys.total_swap(),
                 String::from("SWAP"),
-                color,
-                borders,
-                background
+                config.gauge_color(),
+                config.borders(),
+                config.has_background(),
+                config.text_color()
+
             ), // used SWAP
-            build_colorful_gauge_percent(sys.global_cpu_usage() as u16, String::from("CPU"), color, borders,background), // CPU
+            build_colorful_gauge_percent(
+                sys.global_cpu_usage() as u16,
+                String::from("CPU"),
+                config.gauge_color(),
+                config.borders(),
+                config.has_background(),
+                config.text_color()
+
+                )
         ];
 
         let disks = Disks::new_with_refreshed_list();
@@ -128,9 +184,10 @@ pub mod ui {
                 disk.total_space() - disk.available_space(),
                 disk.total_space(),
                 disk.name().to_str().unwrap_or("Unknow name").to_string(),
-                disk_color,
-                borders,
-                background
+                config.disk_color(),
+                config.borders(),
+                config.has_background(),
+                config.text_color()
             ));
         }
 
--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,19 @@ use std::{
     sync::mpsc,
     thread,
 };
-use system_gauges::ui::colors::get_color_from_string;
+use system_gauges::ui::{colors::get_color_from_string, Configuration};
 use system_gauges::ui::ui;
 
-use termion::{clear, input::TermRead, raw::IntoRawMode, screen::{ToAlternateScreen, ToMainScreen}};
+use termion::{input::TermRead, raw::IntoRawMode, screen::{ToAlternateScreen, ToMainScreen}};
 use tui::{backend::TermionBackend, style::Color, widgets::Borders, Terminal};
 fn main() -> Result<(), io::Error> {
 
     //build Terminal
-    let mut borders = Borders::ALL;
-    let mut background = true;
     let mut end:bool = false;
-    let gauge_colors = parse_arguments(&mut borders, &mut background).unwrap_or_else(|e|{
+    let config = parse_arguments().unwrap_or_else(|e|{
         eprintln!("{e}");
         end = true;
-        (Color::White, Color::White)
+        Configuration::default()
     });
     let stdout = io::stdout().into_raw_mode()?;
     let backend = TermionBackend::new(stdout);
@@ -39,7 +37,7 @@ fn main() -> Result<(), io::Error> {
             break;
         }
         let _ = terminal.draw(|f| {
-            ui(f, gauge_colors.0, gauge_colors.1, &borders, &background);
+            ui(f, &config);
         });
     }
     println!("{ToMainScreen}");
@@ -47,10 +45,13 @@ fn main() -> Result<(), io::Error> {
     Ok(())
 }
 
-fn parse_arguments(borders:& mut Borders, background:&mut bool) -> Result<(Color, Color), String> { // TODO: CHANGE
+fn parse_arguments() -> Result<Configuration, String> { // TODO: CHANGE
     let arguments: Vec<String> = env::args().collect();
     let mut color = Color::White;
     let mut disk_color = Color::White;
+    let mut text_color = Color::White;
+    let mut background = true;
+    let mut borders = Borders::ALL;
     let mut skip = true;
     for (i, arg) in arguments.iter().enumerate() {
         if skip {
@@ -63,7 +64,7 @@ fn parse_arguments(borders:& mut Borders, background:&mut bool) -> Result<(Color
                     
                     return Err(String::from("Please specify a color when you use '-c' "));
                 }
-                color = get_color_from_string(&arguments[i + 1]);
+                color = get_color_from_string(&arguments[i + 1])?;
                 skip = true;
             }
 
@@ -72,26 +73,37 @@ fn parse_arguments(borders:& mut Borders, background:&mut bool) -> Result<(Color
                     return Err(String::from("Please specify a color when you use '-d' "));
 
                 }
-                disk_color = get_color_from_string(&arguments[i + 1]);
+                disk_color = get_color_from_string(&arguments[i + 1])?;
+                skip = true;
+            }
+
+
+            "-t" | "--text" => {
+                if i == arguments.len() - 1 {
+                    return Err(String::from("Please specify a color when you use '-t' "));
+
+                }
+                text_color = get_color_from_string(&arguments[i + 1])?;
                 skip = true;
             }
 
             "-b" | "--borderless" => {
-                *borders = Borders::NONE;
+                borders = Borders::NONE;
             }
 
             "-B" | "--backgroundless" => {
-                *background = false;
+                background = false;
             }
 
             "-h" | "--help" => {
                 let help_menu = "
 ----------------------------------------------
--h | --help           Show this menu
--c | --color          Define the color of the default gauges
--d | --diskcolor      Define the color of the disk gauges
--b | --borderless     Show without borders
--B | --backgroundless Show without background
+-h | --help            Show this menu
+-c | --color           Define the color of the default gauges
+-d | --diskcolor       Define the color of the disk gauges
+-t | --text            Define the color of the text and borders 
+-b | --borderless      Show without borders
+-B | --backgroundless  Show without background
 ----------------------------------------------
 Color List (case insensitive): 
     black,
@@ -111,14 +123,12 @@ Color List (case insensitive):
     lightcyan,
     white
 ";
-                return Err(String::from(help_menu));    
+            return Err(String::from(help_menu));    
             }
             _ => {
-
                 return Err(format!("'{arg}' is not supposed to be there"));    
             }
         }
     }
-
-    Ok((color, disk_color))
+    Ok(Configuration::new(color, disk_color,borders,background,text_color))
 }
